3cbf757e523e055e521cd6f0b5fd6e85b4666a53
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-04 17:44:53 +0100

    wip

--- components/Header/Header.style.js
+++ components/Header/Header.style.js
@@ -2,5 +2,5 @@ import { StyleSheet } from "react-native";
 
 export const s = StyleSheet.create({
   img: { width: 170 },
-  subtitle: { marginTop: -20, color: "#ABABAB" },
+  subtitle: { marginTop: -20, color: "#ABABAB", fontSize: 20 },
 });
