a1bb4613cdbfbdfac95d31fdb905dcd7bc554575
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-12 13:44:11 +0200

    wip

--- App.js
+++ App.js
@@ -2,6 +2,14 @@ import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import { Header } from "./components/Header/Header";
+import { CardTodo } from "./components/CardTodo/CardTodo";
+
+const TODO_LIST = [
+  { id: 1, title: "Walk the dog", isCompleted: true },
+  { id: 2, title: "Go to the dentist", isCompleted: false },
+  { id: 3, title: "Learn React Native", isCompleted: false },
+];
+
 export default function App() {
   return (
     <>
@@ -11,7 +19,7 @@ export default function App() {
             <Header />
           </View>
           <View style={s.body}>
-            <Text>Body</Text>
+            <CardTodo todo={TODO_LIST[1]} />
           </View>
         </SafeAreaView>
       </SafeAreaProvider>
