b25136d615cb240ee8478f9ee1cf9ff163c29ef0
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-13 14:48:10 +0200

    wip

--- App.js
+++ App.js
@@ -1,29 +1,88 @@
-import { ScrollView, Text, View } from "react-native";
+import { Alert, ScrollView, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import { Header } from "./components/Header/Header";
 import { CardTodo } from "./components/CardTodo/CardTodo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TabBottomMenu } from "./components/TabBottomMenu/TabBottomMenu";
+import { ButtonAdd } from "./components/ButtonAdd/ButtonAdd";
+import Dialog from "react-native-dialog";
+import uuid from "react-native-uuid";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+let isFirstRender = true;
+let isLoadUpdate = false;
 
 export default function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: "Walk the dog", isCompleted: true },
-    { id: 2, title: "Go to the dentist", isCompleted: false },
-    { id: 3, title: "Learn React Native", isCompleted: false },
-    { id: 4, title: "Walk the dog", isCompleted: true },
-    { id: 5, title: "Go to the dentist", isCompleted: false },
-    { id: 6, title: "Learn React Native", isCompleted: false },
-    { id: 7, title: "Walk the dog", isCompleted: true },
-    { id: 8, title: "Go to the dentist", isCompleted: false },
-    { id: 9, title: "Learn React Native", isCompleted: false },
-  ]);
-  const [selectedTabName, setSelectedTabName] = useState("inProgress");
+  const [todoList, setTodoList] = useState([]);
+  const [selectedTabName, setSelectedTabName] = useState("all");
+  const [isAddDialogDisplayed, setIsAddDialogDisplayed] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    loadTodoList();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoadUpdate) {
+      if (!isFirstRender) {
+        saveTodoList();
+      } else {
+        isFirstRender = false;
+      }
+    } else {
+      isLoadUpdate = false;
+    }
+  }, [todoList]);
+
+  async function loadTodoList() {
+    console.log("LOAD");
+    try {
+      const todoListString = await AsyncStorage.getItem("@todoList");
+      const parsedTodoList = JSON.parse(todoListString);
+      isLoadUpdate = true;
+      setTodoList(parsedTodoList);
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  async function saveTodoList() {
+    console.log("SAVE");
+    try {
+      await AsyncStorage.setItem("@todoList", JSON.stringify(todoList));
+    } catch (err) {
+      alert(err);
+    }
+  }
+  function getFilteredList() {
+    switch (selectedTabName) {
+      case "all":
+        return todoList;
+      case "inProgress":
+        return todoList.filter((todo) => !todo.isCompleted);
+      case "done":
+        return todoList.filter((todo) => todo.isCompleted);
+    }
+  }
+
+  function deleteTodo(todoToDelete) {
+    Alert.alert("Delete todo", "Are you sure you want to delete this todo ?", [
+      {
+        text: "Delete",
+        style: "destructive",
+        onPress: () => {
+          setTodoList(todoList.filter((t) => t.id !== todoToDelete.id));
+        },
+      },
+      { text: "Cancel", style: "cancel" },
+    ]);
+  }
 
   function renderTodoList() {
-    return todoList.map((todo) => (
+    return getFilteredList().map((todo) => (
       <View key={todo.id} style={s.cardItem}>
-        <CardTodo onPress={updateTodo} todo={todo} />
+        <CardTodo onLongPress={deleteTodo} onPress={updateTodo} todo={todo} />
       </View>
     ));
   }
@@ -40,6 +99,44 @@ export default function App() {
     updatedTodoList[indexToUpdate] = updatedTodo;
     setTodoList(updatedTodoList);
   }
+
+  function addTodo() {
+    const newTodo = {
+      id: uuid.v4(),
+      title: inputValue,
+      isCompleted: false,
+    };
+    setTodoList([...todoList, newTodo]);
+    setIsAddDialogDisplayed(false);
+    setInputValue("");
+  }
+
+  function renderAddDialog() {
+    return (
+      <Dialog.Container
+        visible={isAddDialogDisplayed}
+        onBackdropPress={() => setIsAddDialogDisplayed(false)}
+      >
+        <Dialog.Title>Add todo</Dialog.Title>
+        <Dialog.Description>Choose a name for your todo</Dialog.Description>
+        <Dialog.Input
+          onChangeText={setInputValue}
+          placeholder="Ex : Go to the dentis"
+        />
+        <Dialog.Button
+          label="Cancel"
+          color="grey"
+          onPress={() => setIsAddDialogDisplayed(false)}
+        />
+        <Dialog.Button
+          disabled={inputValue.length === 0}
+          label="Save"
+          onPress={addTodo}
+        />
+      </Dialog.Container>
+    );
+  }
+
   return (
     <>
       <SafeAreaProvider>
@@ -50,14 +147,17 @@ export default function App() {
           <View style={s.body}>
             <ScrollView>{renderTodoList()}</ScrollView>
           </View>
+          <ButtonAdd onPress={() => setIsAddDialogDisplayed(true)} />
         </SafeAreaView>
       </SafeAreaProvider>
       <View style={s.footer}>
         <TabBottomMenu
+          todoList={todoList}
           onPress={setSelectedTabName}
           selectedTabName={selectedTabName}
         />
       </View>
+      {renderAddDialog()}
     </>
   );
 }
--- components/CardTodo/CardTodo.jsx
+++ components/CardTodo/CardTodo.jsx
@@ -1,9 +1,13 @@
 import { Image, Text, TouchableOpacity } from "react-native";
 import { s } from "./CardTodo.style";
 import checkImg from "../../assets/check.png";
-export function CardTodo({ todo, onPress }) {
+export function CardTodo({ todo, onPress, onLongPress }) {
   return (
-    <TouchableOpacity style={s.card} onPress={() => onPress(todo)}>
+    <TouchableOpacity
+      onLongPress={() => onLongPress(todo)}
+      style={s.card}
+      onPress={() => onPress(todo)}
+    >
       <Text
         style={[
           s.title,
