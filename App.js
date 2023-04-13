54961f2b3393c7a68bc6c7f91337de5b4b7a07a2
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-13 14:52:01 +0200

    wip

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import { Alert, ScrollView, Text, View } from "react-native";
+import { Alert, ScrollView, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import { Header } from "./components/Header/Header";
 import { CardTodo } from "./components/CardTodo/CardTodo";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { TabBottomMenu } from "./components/TabBottomMenu/TabBottomMenu";
-import { ButtonAdd } from "./components/ButtonAdd/ButtonAdd";
-import Dialog from "react-native-dialog";
-import uuid from "react-native-uuid";
 
 export default function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Walk the dog", isCompleted: true },
+    { id: 2, title: "Go to the dentist", isCompleted: false },
+    { id: 3, title: "Learn React Native", isCompleted: false },
+    { id: 4, title: "Walk the dog", isCompleted: true },
+    { id: 5, title: "Go to the dentist", isCompleted: false },
+    { id: 6, title: "Learn React Native", isCompleted: false },
+    { id: 7, title: "Walk the dog", isCompleted: true },
+    { id: 8, title: "Go to the dentist", isCompleted: false },
+    { id: 9, title: "Learn React Native", isCompleted: false },
+  ]);
   const [selectedTabName, setSelectedTabName] = useState("all");
-  const [isAddDialogDisplayed, setIsAddDialogDisplayed] = useState(false);
-  const [inputValue, setInputValue] = useState("");
 
   function getFilteredList() {
     switch (selectedTabName) {
@@ -60,43 +65,6 @@ export default function App() {
     setTodoList(updatedTodoList);
   }
 
-  function addTodo() {
-    const newTodo = {
-      id: uuid.v4(),
-      title: inputValue,
-      isCompleted: false,
-    };
-    setTodoList([...todoList, newTodo]);
-    setIsAddDialogDisplayed(false);
-    setInputValue("");
-  }
-
-  function renderAddDialog() {
-    return (
-      <Dialog.Container
-        visible={isAddDialogDisplayed}
-        onBackdropPress={() => setIsAddDialogDisplayed(false)}
-      >
-        <Dialog.Title>Add todo</Dialog.Title>
-        <Dialog.Description>Choose a name for your todo</Dialog.Description>
-        <Dialog.Input
-          onChangeText={setInputValue}
-          placeholder="Ex : Go to the dentis"
-        />
-        <Dialog.Button
-          label="Cancel"
-          color="grey"
-          onPress={() => setIsAddDialogDisplayed(false)}
-        />
-        <Dialog.Button
-          disabled={inputValue.length === 0}
-          label="Save"
-          onPress={addTodo}
-        />
-      </Dialog.Container>
-    );
-  }
-
   return (
     <>
       <SafeAreaProvider>
@@ -107,7 +75,6 @@ export default function App() {
           <View style={s.body}>
             <ScrollView>{renderTodoList()}</ScrollView>
           </View>
-          <ButtonAdd onPress={() => setIsAddDialogDisplayed(true)} />
         </SafeAreaView>
       </SafeAreaProvider>
       <View style={s.footer}>
@@ -117,7 +84,6 @@ export default function App() {
           selectedTabName={selectedTabName}
         />
       </View>
-      {renderAddDialog()}
     </>
   );
 }
